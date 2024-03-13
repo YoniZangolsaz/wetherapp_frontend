96959291b0c6be5a28b910a6eb08111eb172f331
YoniZangolsaz <yoniza770@gmail.com>
2024-03-13 14:47:18 +0200

    add env to google key

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,12 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import { BrowserRouter as Router } from "react-router-dom";
 import { GoogleOAuthProvider } from "@react-oauth/google";
-
 import "./index.css";
+export const googleKey = import.meta.env.VITE_API_GOOGLE_KEY;
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <GoogleOAuthProvider clientId="506324502670-jm8sgkb56t73114q9go7k8n6hg2qj7sb.apps.googleusercontent.com">
+    <GoogleOAuthProvider clientId={googleKey}>
       <Router>
         <App />
       </Router>
